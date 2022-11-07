552e88996d3e22e7d3140b8182902467b393896e
LucasDaviid <lucas_c_david@hotmail.com>
2022-11-07 18:56:18 -0300

    Learning about methods and abstract classes and applying them in an exercise.

---
+++ abstract_methods_and_classes_ex2/src/Main.java
@@ -0,0 +1,55 @@
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<TaxPayer> list = new ArrayList<>();
+
+        System.out.print("Enter the number of tax payers: ");
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++){
+            System.out.println("Tax payer #" + i + " data:");
+            System.out.print("Individual or company (i/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Anual income: ");
+            double anualIncome = sc.nextDouble();
+
+            if (ch == 'i'){
+                System.out.print("Health expenditures: ");
+                double healthExpenditures = sc.nextDouble();
+                list.add(new Individual(name, anualIncome, healthExpenditures));
+            }
+
+            else {
+                System.out.print("Number of employees: ");
+                int numberOfEmployees = sc.nextInt();
+                list.add(new Company(name, anualIncome, numberOfEmployees));
+            }
+        }
+
+        System.out.println("\nTAXES PAID:");
+        double totalTaxes = 0;
+        for (TaxPayer taxPayer:list){
+            System.out.println(taxPayer.getName()+ " - $" + taxPayer.tax());
+            totalTaxes += taxPayer.tax();
+        }
+
+        System.out.println("\nTOTAL TAXES - $" + String.format("%.2f",totalTaxes));
+
+        sc.close();
+    }
+}
---
+++ abstract_methods_and_classes_ex2/src/entities/Individual.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Individual extends TaxPayer{
+
+    private Double healthExpenditures;
+
+    public Individual(String name, Double anualIncome, Double healthExpenditures) {
+        super(name, anualIncome);
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    public Double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+
+    public void setHealthExpenditures(Double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    @Override
+    public Double tax() {
+        if (anualIncome < 20000){
+            return (anualIncome * 15 / 100) - (healthExpenditures * 50 /100) ;
+        }
+        else {
+            return (anualIncome * 25 / 100) - (healthExpenditures * 50 /100) ;
+        }
+    }
+}
